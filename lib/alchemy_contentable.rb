68b632ba2fd01bb747a24c62d5fe00180ff2260f
masche842 <marc@zilia.com>
2012-03-05 13:32:36 +0800

    new requires for namespaced patches in alchemy_contentable

--- lib/alchemy_contentable.rb
+++ lib/alchemy_contentable.rb
@@ -7,12 +7,9 @@ module AlchemyContentable
   require 'alchemy_contentable/controller_mixin'
   require 'alchemy_contentable/migration_helper'
 
-  #require 'patches/resources_helper'
-  #require 'patches/resources_admin_controller'
-
   Engine.config.after_initialize do
-    require 'patches/elements_controller'
-    require 'patches/element.rb'
+    require 'alchemy_contentable/patches/elements_controller'
+    require 'alchemy_contentable/patches/element.rb'
   end
 end
 
