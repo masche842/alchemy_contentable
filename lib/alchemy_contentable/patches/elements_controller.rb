4ec6b21347493997769537de724ae10622c8baa4
masche842 <marc@zilia.com>
2012-03-05 20:12:57 +0800

    fix for new elements on page

--- lib/alchemy_contentable/patches/elements_controller.rb
+++ lib/alchemy_contentable/patches/elements_controller.rb
@@ -88,16 +88,16 @@ module AlchemyContentable
     protected
 
     def load_contentable_to_page
-      if params[:contentable_type] and params[:contentable_id]
+      if params[:contentable_type].present? and params[:contentable_id].present?
         contentable_type = params[:contentable_type]
         contentable_id =params[:contentable_id]
-      elsif params[:element] and params[:element][:contentable_type] and params[:element][:contentable_id]
+      elsif params[:element].present? and params[:element][:contentable_type].present? and params[:element][:contentable_id].present?
         contentable_type = params[:element][:contentable_type]
         contentable_id =params[:element][:contentable_id]
       else
         contentable_type = 'Alchemy::Page'
-        contentable_id = params[:page_id]
-        @page = Alchemy::Page.find(params[:page_id])
+        contentable_id = (params[:page_id].presence or params[:element][:page_id])
+        @page = Alchemy::Page.find(contentable_id)
       end
       contentable_model = contentable_type.classify.constantize
       @page ||= contentable_model.includes(:elements => :contents).find(contentable_id)
